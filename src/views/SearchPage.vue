4aa544f57be8691220c512327f514ee3c61474b8
Sergio Sarmiento <sisarmientob@unal.edu.co>
2020-11-02 18:32:59 -0500

    esqueleto pagina buscar

---
+++ src/views/SearchPage.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="search">
+    <!-- Titulo de la home page -->    
+        <div style=" background-color:#adebad; width:auto; height:100px">
+          <h1 style="background-color: rgb(173, 235, 173); position:absolute; top:5%; width:100%;  margin-bottom:0px; text-align: center;"> knowXchange  </h1>
+        </div>
+        
+        <div>
+            <b-navbar toggleable="lg" type="dark" variant="dark">
+                <b-navbar-brand href="#">Home</b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item-dropdown text="Subject" right>
+                        <b-dropdown-item href="#">Science</b-dropdown-item>
+                        <b-dropdown-item href="#">Tecnology</b-dropdown-item>
+                        <b-dropdown-item href="#">Linguistics</b-dropdown-item>
+                        <b-dropdown-item href="#">Arts</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                    <b-nav-form>
+                    <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form>
+                </b-navbar-nav>
+                
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item-dropdown right>
+                    <!-- Using 'button-content' slot -->
+                    
+                    <template #button-content>
+                        <b-avatar></b-avatar>
+                        <em>User</em>
+                    </template>
+                    <b-dropdown-item href="#">Profile</b-dropdown-item>
+                    <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+
+        <div class="overflow-auto">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage"
+            aria-controls="my-table"
+          ></b-pagination>
+
+          <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+          <b-table
+            id="my-table"
+            :items="items"
+            :per-page="perPage"
+            :current-page="currentPage"
+            small
+          ></b-table>
+      </div>
+
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        perPage: 3,
+        currentPage: 1,
+        items: [
+          { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
+          { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
+          { id: 3, first_name: 'Barney', last_name: 'Rubble' },
+          { id: 4, first_name: 'Betty', last_name: 'Rubble' },
+          { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
+          { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
+          { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
+          { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
+          { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
+        ]
+      }
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      }
+    }
+  }
+</script>
